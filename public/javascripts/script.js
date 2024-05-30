549b9b2523eb07ca54cd15d55bcbfdae41c5ee20
FlixHC <161723849+FlixHC@users.noreply.github.com>
2024-05-30 08:10:45 +0700

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -27,7 +27,6 @@ function openCake(evt, cakeName) {
   }
 
   /* Login page
-  let signupbtn = document.getElementById("signupbtn");
   let signinbtn = document.getElementById("signinbtn");
   let nameField = document.getElementById("nameField");
   let title = document.getElementById("title");
@@ -37,12 +36,5 @@ function openCake(evt, cakeName) {
       title.innerHTML = "Sign In";
       signupbtn.classList.add("disable");
       signinbtn.classList.remove("disable");
-  }
-  
-  signupbtn.onclick = function(){
-      nameField.style.maxHeight = "60px";
-      title.innerHTML = "Sign Up";
-      signupbtn.classList.remove("disable");
-      signinbtn.classList.add("disable");
   }
   */
