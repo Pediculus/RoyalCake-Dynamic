d39f01816582018066d7ad5f9caa0e9658cfc46f
Pediculus <162426933+Pediculus@users.noreply.github.com>
2024-05-30 08:35:37 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET admin page. */
+router.get('/admin/signin', function(req, res, next) {
+  res.render('admin-signin', { title: 'Admin Sign In' });
+});
+
+router.post('/admin/signin', function(req, res, next) {
+  const { username, password } = req.body;
+  
+  if (username === 'admin' && password === 'password') {
+    res.redirect('/admin/dashboard'); // Redirect to admin dashboard on successful sign-in
+  } else {
+    res.render('admin-signin', { title: 'Admin Sign In', error: 'Invalid credentials' });
+  }
+});
+
+/* GET admin-dashboard */
+router.get('/admin/dashboard', function(req, res, next) {
+  res.render('admin-dashboard', { title: 'Admin Dashboard' });
+});
+
 module.exports = router;
