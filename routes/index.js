24fe719f368a7ff3c5f37d614db70017b49be584
Pediculus <162426933+Pediculus@users.noreply.github.com>
2024-06-13 07:34:38 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//import database
+var connection = require('../library/database');
+
+/**
+ * INDEX POSTS
+ */
+router.get('/', function (req, res, next) {
+    //query
+    connection.query('SELECT * FROM cake', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            res.render('index', {
+                data: ''
+            });
+        } else {
+            //render ke view posts index
+            res.render('index', {
+                data: rows // <-- data posts
+            });
+        }
+    });
 });
+
 
 module.exports = router;
